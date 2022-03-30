7a2c72c600d6feb57f5192b747eb609202c4b6ba
abiona01 <quadratwemimo@gmail.com>
2022-03-30 12:19:30 +0100

    scroll update

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -86,6 +86,25 @@
             </div>
           </div>
         </div>
+        <!-- Sender Message-->
+        <div class="media w-50 mb-3"><img src="https://bootstrapious.com/i/snippets/sn-chat/avatar.svg" alt="user" width="50" class="rounded-circle">
+          <div class="media-body ml-3">
+            <div class="bg-light rounded py-2 px-3 mb-2">
+              <p class="text-small mb-0 text-muted">So you do know what you are looking for?</p>
+            </div>
+            <p class="small text-muted">12:00 PM | Aug 13</p>
+          </div>
+        </div>
+
+        <!-- Reciever Message-->
+        <div class="media w-50 ml-auto mb-3">
+          <div class="media-body">
+            <div class="rounded py-2 px-3 mb-2">
+              <button class="btn-primary btn">Yes</button>
+              <button class="btn-danger btn">No</button>
+            </div>
+          </div>
+        </div>
 
       </div>
 
@@ -94,7 +113,7 @@
         <div class="input-group">
           <input type="text" placeholder="Type a message" aria-describedby="button-addon2" class="form-control rounded-0 border-0 py-4 bg-light">
           <div class="input-group-append">
-            <button id="button-addon2" type="submit" class="send-btn"> <i class="fas fa-paper-plane" ></i></button>
+            <button id="button-addon2" type="submit" class="btn btn-link send-btn"> <i class="fas fa-paper-plane" ></i></button>
           </div>
         </div>
       </form>
@@ -107,6 +126,10 @@
 
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script type="text/javascript">
+let chatBox = document.getElementById('chat-box');
+$("#chat-container").scrollTop($("#chat-container")[0].scrollHeight);
+</script>
 <script type="text/javascript">
 
 $('#chat-form').on('submit',function(e){
