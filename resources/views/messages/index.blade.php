556e2591317670be810479a226cde676f019f48a
abiona01 <quadratwemimo@gmail.com>
2022-04-01 16:10:04 +0100

    update

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -1,21 +1,5 @@
 @extends('layouts.frontend')
-@push('styles')
-<link rel="stylesheet" href="{{asset('css/wizard.css')}}">
-<!-- <style>
-     
-      .chat-container {
-        height: 80vh;
-      }
-      #chat-box {
-          overflow-y: scroll;
-          scrollbar-width: none; /* Firefox */
-          -ms-overflow-style: none;  /* Internet Explorer 10+ */
-      }
-  #chat-box::-webkit-scrollbar { /* WebKit */
-      width: 0;
-      height: 0;
-  } -->
-@endpush
+
 @section('main')
 <div class="main">
 
@@ -62,46 +46,12 @@
 @endsection
 
 @push('scripts')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-<script src="{{asset('assets/js/wizard/main.js')}}"></script>
-<script src="{{asset('assets/js/wizard/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('assets/js/wizard/jquery-steps/jquery.steps.js')}}"></script>
-<script src="{{asset('assets/js/wizard/jquery-validation/dist/jquery.validate.min.js')}}"></script>
-<script src="{{asset('assets/js/wizard/jquery-validation/dist/additional-methods.min.js')}}"></script>
-<script type="text/javascript">
-let chatBox = document.getElementById('chat-box');
-$("#chat-container").animate({
-    scrollTop: ($("#chat-container")[0].scrollHeight)
-}, 2500);
-</script>
-<script type="text/javascript">
-$('#chat-form').on('submit', function(e) {
-    e.preventDefault();
-    var message = $('input[type=text]').val();
-    $.ajax({
-        url: "{{ route('storeMessage') }}",
-        dataType: 'json',
-        type: 'POST',
-        data: {
-            '_token': $('meta[name="csrf-token"]').attr('content'),
-            message: message
-        },
-        success: function(data) {
-            // console.log(data)
-            // console.log(data.date)
-            $('input[type=text]').val('');
-            $('#chat-box').append(
-                '<div class="media w-50 ml-auto mb-3">' +
-                '<div class="media-body">' +
-                '<div class="bg-primary rounded py-2 px-3 mb-2">' +
-                '<p class="text-small mb-0 text-white">' + data.message + '</p>' +
-                '</div>' +
-                '<p class="small text-muted">' + data.time + ' | ' + data.date + '</p>' +
-                '</div>' +
-                '</div>'
-            );
-        }
-    });
-});
-</script>
+
+<script src="{{asset('js/wizard/main.js')}}"></script>
+<script src="{{asset('js/wizard/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('js/wizard/jquery-steps/jquery.steps.js')}}"></script>
+<script src="{{asset('js/wizard/jquery-validation/dist/jquery.validate.min.js')}}"></script>
+<script src="{{asset('js/wizard/jquery-validation/dist/additional-methods.min.js')}}"></script>
+
+
 @endpush
